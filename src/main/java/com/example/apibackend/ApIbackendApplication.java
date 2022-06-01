4c5e609fd632c2e3500d412bbd05c0d90dd46879
TimoOerlemans <t.oerlemans@student.fontys.nl>
2022-06-01 09:28:12 +0200

    project toegevoegd

---
+++ src/main/java/com/example/apibackend/ApIbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.apibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApIbackendApplication.class, args);
+    }
+
+}
